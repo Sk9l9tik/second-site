fe22dedb521f08b03bd1426e9ff9514457186de9
sk9l9tik <belosheev_ad@edu.surgu.ru>
2024-04-04 21:39:28 +0500

    test direction 2

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,15 @@
-
+/*
 import Swiper from '../node_modules/swiper/swiper-bundle.min.mjs';
 
 import Navigation from '../node_modules/swiper/modules/navigation.min.mjs';
 import Pagination from '../node_modules/swiper/modules/pagination.min.mjs';
+*/
 
-/*
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 import Navigation from 'https://cdn.jsdelivr.net/npm/swiper@11/modules/navigation.min.mjs';
 import Pagination from 'https://cdn.jsdelivr.net/npm/swiper@11/modules/pagination.min.mjs';
-*/
+
 
 
 const swp = document.querySelector(".swiper1");
