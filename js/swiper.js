17476a18d14f77792badc072cbfa2c13f4f96fc6
sk9l9tik <belosheev_ad@edu.surgu.ru>
2024-04-06 22:46:47 +0500

    Full adaptiv and swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,15 +1,15 @@
-/*
+
 import Swiper from '../node_modules/swiper/swiper-bundle.min.mjs';
 
 import Navigation from '../node_modules/swiper/modules/navigation.min.mjs';
 import Pagination from '../node_modules/swiper/modules/pagination.min.mjs';
-*/
 
+/*
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
 import Navigation from 'https://cdn.jsdelivr.net/npm/swiper@11/modules/navigation.min.mjs';
 import Pagination from 'https://cdn.jsdelivr.net/npm/swiper@11/modules/pagination.min.mjs';
-
+*/
 
 
 const swp = document.querySelector(".swiper1");
@@ -17,7 +17,7 @@ const swp = document.querySelector(".swiper1");
 const swp_slides = document.querySelectorAll(".swiper-slide1");
 
 function handleResize(){
-  console.log(window.screen.width < 999);
+  //console.log(window.screen.width < 999);
 
   if( window.screen.width < 999){
     swp.classList.add("swiper");
@@ -54,7 +54,7 @@ function swiper1(){
     // configure Swiper to use modules
     modules: [Navigation, Pagination],
     direction: 'horizontal',
-    loop: true,
+    //loop: true,
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
